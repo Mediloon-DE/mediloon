09e1ceb589e28eebcec4e378708136a0021d407a
sambhavmediloon <sambhav.j@Mediloon2.onmicrosoft.com>
2025-07-25 11:10:14 +0530

    fix error

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,12 +1,16 @@
-import { Suspense } from 'react';
+"use client"
+
+import { Suspense, use } from 'react';
 import SearchResults from '@/components/Search/SearchResults';
 import Loader from '@/components/Common/Loader';
 
-export default function SearchPage({
-    searchParams,
-}: {
-    searchParams: { q: string };
+type Params = Promise<{ q: string }>
+
+export default function SearchPage(props: {
+    params: Params
 }) {
+    const params = use(props.params)
+    const q = params.q
     return (
         <div className="container mx-auto px-4 py-8">
             <h1 className="text-2xl font-bold mb-6">
@@ -14,7 +18,7 @@ export default function SearchPage({
             </h1>
 
             <Suspense fallback={<Loader />}>
-                <SearchResults searchQuery={searchParams.q} />
+                <SearchResults searchQuery={q} />
             </Suspense>
         </div>
     );
